8c3251c781fe8fcb4fae87be7b99d51b1e80e609
Flesch_c <fleschcyril@gmail.com>
2016-10-26 15:18:19 +0200

    init map

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/websocket"
+	"fmt"
 )
+
+type mapData struct {
+	empty, playable bool
+	team int
+}
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
@@ -38,7 +44,27 @@ func echo(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func initMap() ([]mapData) {
+
+	myMap := make([]mapData, 19 * 19)
+	for  x := 0; x < 19 * 19; x++ {
+		myMap[x].empty = true
+		myMap[x].playable = true
+		myMap[x].team = -1
+	}
+	return myMap
+}
+
+
 func main() {
+
+	myMap := initMap()
+
+	for  x := 0; x < 19*19; x++ {
+		fmt.Println("x:", x, " sum:", myMap[x].team) // Simple output.
+
+	}
+
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
