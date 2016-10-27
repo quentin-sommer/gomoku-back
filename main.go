3434e8371c8d166d91aa1fadff4031c56e60cb71
Quentin Sommer <sommerquentin67@gmail.com>
2016-10-27 20:15:13 +0200

    ping pong routine

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/websocket"
-	"fmt"
-	"encoding/json"
 )
 
 type mapData struct {
@@ -28,6 +26,8 @@ var messageTypes = [...]string{
 	"PLAY_TURN",
 	"END_OF_GAME",
 }
+var nbSockets = 0
+var sockets [2]*websocket.Conn
 
 func (messageType MessageType) String() string {
 	return messageTypes[messageType - 1]
@@ -36,6 +36,10 @@ func (messageType MessageType) String() string {
 type Message struct {
 	Type string
 	//	gameMap     mapData
+}
+type MessageStartOfGame struct {
+	Type         string
+	PlayerNumber int
 }
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
@@ -47,17 +51,51 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func sendTestMessage(t MessageType) ([]byte) {
-	msg := &Message{
+func sendMessage(t MessageType) (*Message) {
+	return &Message{
 		t.String(),
 	}
+}
 
-	b, err := json.Marshal(msg)
-	if err != nil {
-		fmt.Println("sendTestMessage:", err)
-		return nil
+func sendStartOfGame(number int) (*MessageStartOfGame) {
+	return &MessageStartOfGame{
+		"START_OF_GAME",
+		number}
+}
+
+func initGameRoutine() {
+	sockets[0].WriteJSON(sendStartOfGame(0))
+	sockets[1].WriteJSON(sendStartOfGame(1))
+}
+
+func gameHasEnded() bool {
+	return true
+}
+
+func playTurn(c *websocket.Conn) bool {
+	c.WriteJSON(sendMessage(PLAY_TURN))
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+
+		if !gameHasEnded() {
+			return false
+		} else {
+			return true
+		}
 	}
-	return b
+	return false
+}
+func gameRoutine() {
+	//	gameHasEnded := false
+	for {
+		playTurn(sockets[0])
+		playTurn(sockets[1])
+	}
 }
 
 func echo(w http.ResponseWriter, r *http.Request) {
@@ -66,20 +104,24 @@ func echo(w http.ResponseWriter, r *http.Request) {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		msg := sendTestMessage(START_OF_GAME)
-		err = c.WriteMessage(mt, msg)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
+	switch nbSockets {
+	// first connected
+	case 0:
+		sockets[0] = c
+		nbSockets += 1
+		log.Print(nbSockets + " socket connected")
+		c.WriteJSON(sendMessage(IDLE))
+		break
+	// all sockets connected
+	case 1:
+		sockets[1] = c
+		nbSockets += 1
+		log.Print("starting game")
+		initGameRoutine()
+		gameRoutine()
+		break
+	default:
+		break
 	}
 }
 
@@ -96,15 +138,16 @@ func initMap() ([]mapData) {
 
 func main() {
 
-	myMap := initMap()
+	//myMap := initMap()
 
 	for x := 0; x < 19 * 19; x++ {
-		fmt.Println("x:", x, " sum:", myMap[x].team) // Simple output.
+		//	fmt.Println("x:", x, " sum:", myMap[x].team) // Simple output.
 
 	}
 
 	flag.Parse()
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
+	log.Println("Server start")
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
