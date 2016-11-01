9666b5e246214857080af2a3468d46beb9d2aacb
Quentin Sommer <sommerquentin67@gmail.com>
2016-11-01 17:58:22 +0100

    changed branch for frontend build to master

--- build_fontend.sh
+++ build_fontend.sh
@@ -3,7 +3,6 @@ rm -rf public/*
 
 git clone git@github.com:quentin-sommer/gomoku-front.git \
   && cd gomoku-front \
-  && git checkout develop \
   && yarn install \
   && echo "Building application..." \
   && npm run build &>/dev/null \
