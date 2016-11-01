b848cc57275454605fa5392fe4841c7bdb624bcd
Quentin Sommer <sommerquentin67@gmail.com>
2016-11-01 17:32:44 +0100

    frontend build script

--- build_fontend.sh
+++ build_fontend.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 rm -rf public/*
-git clone git@github.com:quentin-sommer/gomoku-front.git
-cd gomoku-front
-yarn install || npm install
-npm run build
-cp -r build/* ././public/
-cd ..
-rm -rf gomku-front
+
+git clone git@github.com:quentin-sommer/gomoku-front.git \
+  && cd gomoku-front \
+  && git checkout develop \
+  && yarn install \
+  && echo "Building application..." \
+  && npm run build &>/dev/null \
+  && cp -r build/* ./../public/ \
+  && cd .. \
+  && rm -rf gomoku-front \
+  && echo "Application built!"
