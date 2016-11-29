0f73acf240bf560066575bdebbde6d6e46e16477
Flesch_c <fleschcyril@gmail.com>
2016-11-29 11:30:36 +0100

    max

--- ia/minmax.go
+++ ia/minmax.go
@@ -44,12 +44,33 @@ func Min(m []protocol.MapData, player int, depth int) (int) {
       }
     }
   }
-
   return min_val
 }
 
-func Max(m []protocol.MapData, player int, profondeur int) (int) {
-  return -1
+func Max(m []protocol.MapData, player int, depth int) (int) {
+
+  if (depth == 0) {
+    return (1) // return value of the move
+  }
+
+  tmpmap := make([]protocol.MapData, len(m))
+  copy(tmpmap, m)
+  max_val := NON_INIT
+  ok := false
+
+  for i := 0; i < MAP_SIZE; i++ {
+    // Simuler coup
+    tmpmap, _, _, ok = referee.Exec(tmpmap, i)
+
+    if (ok) {
+      val := Min(tmpmap, player, depth - 1)
+
+      if (val > max_val || max_val == NON_INIT) {
+        max_val = val
+      }
+    }
+  }
+  return max_val
 }
 
 func MinMax(m []protocol.MapData, player int, profondeur int) (int, int) {
