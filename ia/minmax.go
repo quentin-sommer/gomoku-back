60a53ce9338d6bb7ed5371d2016f60aa6c2ca115
Flesch_c <fleschcyril@gmail.com>
2016-11-29 10:24:06 +0100

    min max param

--- ia/minmax.go
+++ ia/minmax.go
@@ -13,11 +13,19 @@ const (
   THREE_ALIGN = 2
   FOUR_ALIGN = 3
   // Compute : base + pawn taken
-  BASE_PAWN_TAKEN = 4 * 10
+  BASE_PAWN_TAKEN = 4
   // Most important, wins over the rest every time
   FIVE_ALIGN = 500
 )
 
-func MinMax(m []protocol.MapData, player int) (int, int) {
+func Min(m []protocol.MapData, player int, profondeur int) (int) {
+  return -1
+}
+
+func Max(m []protocol.MapData, player int, profondeur int) (int) {
+  return -1
+}
+
+func MinMax(m []protocol.MapData, player int, profondeur int) (int, int) {
   return -1, -1
 }
