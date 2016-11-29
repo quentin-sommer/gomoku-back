a00af7d018a786c96bcc40e6e5cf9d9a247f40ba
Quentin Sommer <sommerquentin67@gmail.com>
2016-11-29 10:19:14 +0100

    ia constants

--- ia/minmax.go
+++ ia/minmax.go
@@ -5,6 +5,19 @@ import (
   "./../protocol"
 )
 
+/*
+ * We start off by guiding ai to build up sequences, and then emphasis on taking pawns
+ */
+const (
+  TWO_ALIGN = 1
+  THREE_ALIGN = 2
+  FOUR_ALIGN = 3
+  // Compute : base + pawn taken
+  BASE_PAWN_TAKEN = 4 * 10
+  // Most important, wins over the rest every time
+  FIVE_ALIGN = 500
+)
+
 func MinMax(m []protocol.MapData, player int) (int, int) {
   return -1, -1
 }
