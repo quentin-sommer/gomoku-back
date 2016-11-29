8fbc39313f120aa733912da99d9161beafa44c84
Quentin Sommer <sommerquentin67@gmail.com>
2016-11-29 11:17:30 +0100

    fixes

--- ia/minmax.go
+++ ia/minmax.go
@@ -16,28 +16,30 @@ const (
   BASE_PAWN_TAKEN = 4
   // Most important, wins over the rest every time
   FIVE_ALIGN = 500
+  MAP_SIZE = 19 * 19
+  NON_INIT = -42
 )
 
-func Min(m []protocol.MapData, player int, profondeur int) (int) {
+func Min(m []protocol.MapData, player int, depth int) (int) {
 
-  if (profondeur == 0) {
+  if (depth == 0) {
     return (1) // return value of the move
   }
 
   tmpmap := make([]protocol.MapData, len(m))
   copy(tmpmap, m)
-  min_val := 0
+  min_val := NON_INIT
   ok := false
 
-  for i := 0; i < 19 * 19 ; i++  {
+  for i := 0; i < MAP_SIZE; i++ {
     // Simuler coup
     tmpmap, _, _, ok = referee.Exec(tmpmap, i)
 
     if (ok) {
 
-      val := Max(tmpmap, player, profondeur - 1)
+      val := Max(tmpmap, player, depth - 1)
 
-      if (val < min_val && min_val != 0) {
+      if (val < min_val || min_val == NON_INIT) {
         min_val = val
       }
     }
